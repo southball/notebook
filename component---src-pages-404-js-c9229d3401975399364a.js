5f7ef1f092ba490fd9fd0d1d34e7a0fae857d747
Southball <jamie.choi.mail.2002@gmail.com>
2020-02-21 17:46:36 +0800

    Updates

---
+++ component---src-pages-404-js-c9229d3401975399364a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),c=n("vrFN");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(c.a,{title:"404: Not found"}),l.a.createElement("div",{className:"content"},l.a.createElement("h1",null,"404 Page Not Found"),l.a.createElement("p",null,"The page you are trying to access is not found.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c9229d3401975399364a.js.map
